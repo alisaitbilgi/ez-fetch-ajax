fc3d36a34967a841c378e8d8076b4b585ec4e0a8
ali sait bilgi <alisaitbilgi@gmail.com>
2017-03-01 12:12:33 +0300

    Add files via upload

---
+++ build/Request.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var handler = {
+    get: function get(target, name) {
+        return name in target ? target[name] : function () {
+            var url = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "";
+            var data = arguments[1];
+
+            var method = name.toUpperCase();
+            return new Promise(function (res, rej) {
+                var xhr = new XMLHttpRequest();
+                xhr.open(method, url, true);
+                xhr.send(data);
+                xhr.onload = function () {
+                    res([xhr.status, xhr.response]);
+                };
+                xhr.onerror = function (e) {
+                    rej(new Error(e));
+                };
+            });
+        };
+    }
+};
+
+var Request = new Proxy({}, handler);
+
+module.exports = { Request: Request };
+
---
+++ src/Request.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const handler = {
+    get: function (target, name) {
+        return name in target ? target[name] : function (url="", data) {
+                let method = name.toUpperCase();
+                return new Promise((res, rej) => {
+                    const xhr = new XMLHttpRequest();
+                    xhr.open(method, url, true);
+                    xhr.send(data);
+                    xhr.onload = function () {
+                        res([xhr.status, xhr.response]);
+                    };
+                    xhr.onerror = function (e) {
+                        rej(new Error(e));
+                    };
+                });
+            };
+    }
+};
+
+const Request = new Proxy({}, handler);
+
+module.exports = {Request};
